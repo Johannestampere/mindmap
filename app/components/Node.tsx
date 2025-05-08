901cea8204d4b5d5bcabddebc47b3759dde99da1
Johannes Tampere <tamperejohannes@gmail.com>
2025-05-08 10:40:03 -0400

    fe setup

---
+++ app/components/Node.tsx
@@ -0,0 +1,3 @@
+export default function Node() {
+    <div>Node</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 export default function LandingPage() {
   return (
     <div>
-      <h1>AI-Generated Mindmaps</h1>
-      <div>text</div>
+      <h1 className="text-green-600">AI-Generated Mindmaps</h1>
+      <div>Log In</div>
     </div>
   );
 }
