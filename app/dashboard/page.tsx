1343f83aeb12b7604dbe9f4bde82cdefd07fd73e
Johannes Tampere <tamperejohannes@gmail.com>
2025-05-09 17:19:27 -0400

    auth setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React, { JSX } from "react";
+
+export default function Dashboard(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-6xl">user dashboard</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import React, { JSX } from "react";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
 export default function LandingPage(): JSX.Element {
+  
   return (
     <div>
-      <h1 className="text-green-600">AI-Generated Mindmaps</h1>
-      <div>Log In</div>
+      <h1>Sign in to your account</h1>
+      <form>
+
+        <div>
+          <input placeholder="email"></input>
+        </div>
+
+        <div>
+          <input placeholder="password"></input>
+        </div>
+
+        <button>Forgot password?</button>
+
+        <button>Sign in</button>
+
+        <div>Or sign in with</div>
+
+        <div>Google</div>
+        <div>Github</div>
+      </form>
     </div>
   );
 }
