503d58401076e2c7e4d82e7081bee9afeee96403
Johannes Tampere <tamperejohannes@gmail.com>
2025-05-10 18:10:32 -0400

    supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home() {
+import Link from "next/link";
+import React from "react";
+import LoginButton from "./components/LoginButton";
+
+export default function LandingPage() {
   return (
-    <div>Landingpage</div>
+    <div>
+      <h1>landing page</h1>
+      <LoginButton />
+    </div>
   )
 }
