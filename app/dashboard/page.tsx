262268dffd39023b94201914b5f8fd1390274ae9
Johannes Tampere <tamperejohannes@gmail.com>
2025-05-17 21:46:55 -0400

    get mindmaps route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,9 +11,29 @@ export default async function Dashboard() {
     redirect('/login')
   }
 
+  const userId = data.user.id
+
+  // req: send user id to backend, get back all mindmap id's of that user
+  const res = await fetch("http://hfcs.csclub.uwaterloo.ca:8000/get_mindmaps", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_id: userId,
+    }),
+  })
+
+  const mindmaps = await res.json()
+
   return (
     <div>
       <h1 className="text-6xl">{data.user.email}</h1>
+      <ul>
+          {mindmaps.map((mindmap: { id: string }) => (
+            <li key={mindmap.id}>{mindmap.id}</li>
+          ))}
+      </ul>
       <LogOutButton />
     </div>
   )
